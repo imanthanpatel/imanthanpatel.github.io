b16711e4c7d0d2bd70ea0138ab5b435d0bb716f0
Manthan Patel <manthan002408@gmail.com>
2025-05-03 16:30:50 +0530

    hii

---
+++ portfolio/src/Card.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+function Card() {
+  return (
+    <div className="card">
+      <div className="class1">
+      <h1 className="Manthan">Manthan Patel</h1>
+      <h2 className="devloper">Frontend Devloper</h2>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Card;
